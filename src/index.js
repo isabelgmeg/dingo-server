611e6fee9cf2efcb4b8e16f4cef7c17b735ea845
Isabelgarcia <isabelgmegino@gmail.com>
2021-04-29 21:57:34 +0200

    configs/constants created  with PORT added as a variable

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const app = express()
 
+const {PORT} = require('./configs/constants')
+
 app.listen(3000, () => {
-  console.log('App listening in http://localhost:3000')
+  console.log(`'App listening in http://localhost:${PORT}'`)
 }) 
