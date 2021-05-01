5a05d3c89cf0d9847e204a5f0174f368c32880d6
Isabelgarcia <isabelgmegino@gmail.com>
2021-05-01 19:38:38 +0200

    half data populated

--- scripts/recipes.js
+++ scripts/recipes.js
@@ -4,6 +4,24 @@ const IngredientsModel = require('../src/models/Ingredients');
 const dummyImages = require('./seeds/imagesDummy');
 const dummyRecipes = require('./seeds/recipesDummy.json');
 
+const selectRandomIngredient = (array) =>
+  array[Math.floor(Math.random() * array.length)];
+
+const populateRecipes = (recipesArr, ingredientsArr) =>
+  recipesArr.forEach((recipe) => {
+    const randomQuantityIngredients = Math.floor(2 + Math.random() * 8);
+    populateIngredients(ingredientsArr, randomQuantityIngredients, recipe);
+  });
+
+const populateIngredients = (ingredientsArr, randomQuantityEle, recipe) => {
+  for (let i = 0; i < randomQuantityEle; i++) {
+    const selectedIngredient = selectRandomIngredient(ingredientsArr);
+    if (!recipe.ingredients.includes(selectedIngredient)) {
+      recipe.ingredients.push(selectedIngredient);
+    }
+  }
+};
+
 const createRecipes = async () => {
   const ingredients = await IngredientsModel.find({});
 
@@ -14,35 +32,24 @@ const createRecipes = async () => {
 
     ingredientsArray.push(id);
     //console.info(ingredientsArray);
-
-    const newRecipes = dummyRecipes.map((recipe) => ({
-      ...recipe,
-      ingredients: [],
-      mealType: '',
-      intolerances: [],
-      picture: '',
-      createdBy: 'dingo',
-    }));
-
-    console.info('newRecipes', newRecipes);
   }
+  const newRecipes = dummyRecipes.map((recipe) => ({
+    ...recipe,
+    ingredients: [],
+    mealType: '',
+    intolerances: [],
+    picture: '',
+    createdBy: 'dingo',
+  }));
+
+  //console.info('newRecipes', newRecipes);
 
   //return newRecipes;
 
-  const populateRecipes = (recipesArr, ingredientsArr) =>
-    recipesArr.forEach((recipe) => {
-      const randomQuantityIngredients = Math.floor(2 + Math.random() * 8);
-      populateIngredients(ingredientsArr, randomQuantityIngredients, recipe);
-    });
+  populateRecipes(newRecipes, ingredientsArray);
 
-  const populateIngredients = (ingredientsArr, randomQuantityEle, recipe) => {
-    for (let i = 0; i < randomQuantityEle; i++) {
-      const selectedIngredient = selectRandomIngredient(ingredientsArr);
-      if (!recipe.ingredients.includes(selectedIngredient)) {
-        recipe.ingredients.push(selectedIngredient);
-      }
-    }
-  };
+  console.info(newRecipes);
+  return newRecipes;
 };
 
 module.exports = {
