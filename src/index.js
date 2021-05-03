6e8962c3a0b2dea38598e095088090642662e560
Isabelgarcia <isabelgmegino@gmail.com>
2021-05-03 09:35:03 +0200

    middlewares added, not using bodyParser and using express

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 const express = require('express')
+const cors = require('cors')
+const { PORT } = require('./configs/constants')
 
 const app = express()
 
 require("./configs/db")
-const { PORT } = require('./configs/constants')
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+app.use(cors())
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
+
+app.use((req, res, next) => {
+  next(new Error("Path Not Found"));
+});
+
+app.use((error, _, res, __) => {
+  res.status(400).json({
+    success: false,
+    message: error.message,
+  });
+});
 
 app.listen(PORT, () => console.info(`> Listening at http://localhost:${PORT}`));
