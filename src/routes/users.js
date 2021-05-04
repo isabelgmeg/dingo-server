866da6aed602305ba5a58fdfa1bfebc761e77f7d
Isabelgarcia <isabelgmegino@gmail.com>
2021-05-04 21:03:35 +0200

    added auth from user with login, logout, short profile

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,8 +3,9 @@ const omitBy = require('lodash/omitBy');
 
 const UserModel = require('../models/Users');
 
+const { isAuthenticated } = require('../middlewares/authentication');
 
-router.put('/modify', async (req, res, next) => {
+router.put('/modify', [isAuthenticated], async (req, res, next) => {
   try {
     const user = await UserModel.findById(req.user);
 
@@ -14,11 +15,9 @@ router.put('/modify', async (req, res, next) => {
       throw error;
     }
 
-    //const filteredUser = omitBy(req.body, (value, _) => !value);
-    //const result = await UserModel.findByIdAndUpdate(req.user, filteredUser, {
+    const filteredUser = omitBy(req.body, (value, _) => !value);
 
-
-    const result = await UserModel.findByIdAndUpdate(req.user, {
+    const result = await UserModel.findByIdAndUpdate(req.user, filteredUser, {
       new: true
     });
 
@@ -31,22 +30,22 @@ router.put('/modify', async (req, res, next) => {
   }
 });
 
-// router.get('/profile', [isAuthenticated], async (req, res, next) => {
-//   try {
-//     const userId = req.user;
+router.get('/profile', [isAuthenticated], async (req, res, next) => {
+  try {
+    const userId = req.user;
 
-//     const result = await UserModel.findById(userId, { password: 0 });
+    const result = await UserModel.findById(userId, { password: 0 });
 
-//     if (!result) {
-//       const error = new Error('User not found');
-//       error.code = 404;
-//       throw error;
-//     }
+    if (!result) {
+      const error = new Error('User not found');
+      error.code = 404;
+      throw error;
+    }
 
-//     res.status(200).json({ success: true, data: result });
-//   } catch (error) {
-//     res.status(401).json({ success: false, data: error.message });
-//   }
-// });
+    res.status(200).json({ success: true, data: result });
+  } catch (error) {
+    res.status(401).json({ success: false, data: error.message });
+  }
+});
 
 module.exports = router;
