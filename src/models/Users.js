e5c4b350c8740e3de21643486e62210adf2226dd
Isabelgarcia <isabelgmegino@gmail.com>
2021-05-04 19:08:59 +0200

    auth completed *see dummy user*

--- src/models/Users.js
+++ src/models/Users.js
@@ -28,7 +28,7 @@ const UserSchema = new Schema(
       unique: true,
       validate: {
         validator: function (v) {
-          return /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v);
+          return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
         },
         message: "Please enter a valid email",
       },
